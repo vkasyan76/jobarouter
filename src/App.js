a59692c00eaad035025050bdbfb18a838ac75eb6
vkasyan76 <vkasyan@gmx.de>
2023-01-14 13:09:20 +0100

    React Router Basics - old way

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+        doloremque ullam ducimus, architecto officiis quod! Quasi quis maiores
+        eum tenetur rem. Ducimus veniam sunt dignissimos sequi. Excepturi cum
+        modi atque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus at
+        tempore impedit! Rem sit placeat reiciendis doloremque! Iste corporis
+        dicta ea blanditiis possimus, harum eum? Facere enim non dolor
+        perspiciatis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, sapiente
+        fugit magni molestias et, libero nesciunt sunt laudantium architecto at
+        officia dolores totam quam delectus autem? Temporibus, necessitatibus.
+        Sequi, nostrum.
+      </p>
+    </div>
+  )
+}
