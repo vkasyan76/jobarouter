f1de7fd006a7e3e33eb588372738689b320cebc1
vkasyan76 <vkasyan@gmx.de>
2023-01-19 10:32:28 +0100

    Nested Routes and Layouts

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom'
+import HelpLayout from './layouts/HelpLayout'
 import RootLayout from './layouts/RootLayout'
 // pages
 import About from './pages/About'
 import Home from './pages/Home'
+import Contact from './pages/help/Contact'
+import Faq from './pages/help/Faq'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>,
   ),
 )
---
+++ src/layouts/HelpLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet, NavLink } from 'react-router-dom'
+
+export default function HelpLayout() {
+  return (
+    <div className="help-layout">
+      <h2>Website Help</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, itaque.
+      </p>
+
+      <nav>
+        <NavLink to="faq">View the FAQ</NavLink>
+        <NavLink to="contact">Contact Us</NavLink>
+      </nav>
+
+      <Outlet />
+    </div>
+  )
+}
